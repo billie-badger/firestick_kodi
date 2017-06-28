7a7e62794e0de0006d062304dbfd920d950e130a
billie-badger <will.vatcher@gmail.com>
2017-06-27 18:02:40 -0700

    Add files via upload

---
+++ Firestick.bat
@@ -0,0 +1,4 @@
+C:\
+cd (Enter the location of ADB SDK ending with platform-tools and delete parenthesis)
+adb connect (Enter IP address and delete parenthesis)
+adb install kodi-17.3-Krypton-armeabi-v7a.apk
